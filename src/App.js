2251819e9a21a8d6f29ab7cc9b373805a4c72660
Betrayer <StarDestroyer@ukr.net>
2020-06-24 23:04:09 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <h2>I am app {"&"} main</h2>
-    <h2>bohdan was here</h2>
+    <>
+      <h2>I am app {"&"} main</h2>
+      <h2>bohdan was here</h2>
+    </>
   );
-}
+};
 
 export default App;
