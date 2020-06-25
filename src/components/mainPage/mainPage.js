724a7c896c30b57d07e7d7c11e39370d50ee77c2
Betrayer <58057555+Betrayer@users.noreply.github.com>
2020-06-25 17:17:46 +0300

    Bohdan branch (#8)
    * test
    
    * test
    
    * done sectioning
    
    * container fixed
    
    * router test
    
    * oh my blog

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ServicesSection from './servicesSection/ServicesSection';
+import BlogSection from './blogSection/BlogSection';
 
 const mainPage = () => {
     return (
         <>
             <ServicesSection />
+            <BlogSection />
         </>
     );
 }
