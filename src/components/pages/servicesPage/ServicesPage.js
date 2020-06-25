c3d788f574bf808f817cc3653dc23e48168b160b
Betrayer <58057555+Betrayer@users.noreply.github.com>
2020-06-25 12:42:24 +0300

    Bohdan branch (#7)
    * test
    
    * test
    
    * done sectioning
    
    * container fixed
    
    * router test

---
+++ src/components/pages/servicesPage/ServicesPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "./servicesPage.module.css";
+
+const ServicesPage = () => {
+  return (
+    <>
+      <h2 className={css.header}>I am services page</h2>
+    </>
+  );
+};
+
+export default ServicesPage;
