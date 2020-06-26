e102411ced0270bb35268c4fe8a42c029ef7a98f
Betrayer <StarDestroyer@ukr.net>
2020-06-26 15:15:40 +0300

    minor fixes

--- src/components/mainPage/blogSection/BlogSection.js
+++ src/components/mainPage/blogSection/BlogSection.js
@@ -6,46 +6,7 @@ import posts from "../../../postsRus.json";
 import css from "./blogSection.module.css";
 
 const BlogSection = () => {
-  const [isFetching, setIsFetching] = useState(false);
   const [totalPosts, setTotalPosts] = useState([]);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  useEffect(() => {
-    if (!isFetching) return;
-    fetchMoreListItems();
-  }, [isFetching]);
-
-  useEffect(() => {
-    fetchMoreListItems();
-  }, []);
-
-  function handleScroll() {
-    const windowHeight =
-      "innerHeight" in window
-        ? window.innerHeight
-        : document.documentElement.offsetHeight;
-    const body = document.body;
-    const html = document.documentElement;
-    const docHeight = Math.max(
-      body.scrollHeight,
-      body.offsetHeight,
-      html.clientHeight,
-      html.scrollHeight,
-      html.offsetHeight
-    );
-    const windowBottom = windowHeight + window.pageYOffset;
-    if (windowBottom >= docHeight - 300) setIsFetching(true);
-  }
-
-  function fetchMoreListItems() {
-    const preloader = 5 + Number(totalPosts.length);
-    setTotalPosts(posts.filter((item, index) => index <= preloader));
-    setIsFetching(false);
-  }
 
   return (
     <section className={css.blogSection}>
