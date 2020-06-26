45335726047a805c297a6b93c9edf1afaf5bd303
romanitwork <workromanmail@gmail.com>
2020-06-26 11:39:14 +0300

    html stack for sections

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ServicesSection from './servicesSection/ServicesSection';
 import BlogSection from './blogSection/BlogSection';
+import WhyUsSection from './whyUsSection/WhyUsSection';
+import WeWorkSection from './weWorkSection/WeWorkSection';
 
 const mainPage = () => {
     return (
         <>
             <ServicesSection />
+            <WhyUsSection />
+            <WeWorkSection />
             <BlogSection />
         </>
     );
---
+++ src/components/mainPage/whyUsSection/WhyUsSection.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styles from "./whyussection.module.css";
+
+const WhyUsSection = () => {
+  return (
+    <section className={styles.whyus_container}>
+      <h2 className={styles.whyus_title}>Why work with us</h2>
+      <ul className={styles.whyus_list}>
+        <li className={styles.whyus_item}>
+          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
+          <p className={styles.whyus_item_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+        <li className={styles.whyus_item}>
+          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
+          <p className={styles.whyus_item_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+        <li className={styles.whyus_item}>
+          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
+          <p className={styles.whyus_item_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+        <li className={styles.whyus_item}>
+          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
+          <p className={styles.whyus_item_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+        <li className={styles.whyus_item}>
+          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
+          <p className={styles.whyus_item_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+        <li className={styles.whyus_item}>
+          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
+          <p className={styles.whyus_item_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default WhyUsSection;
