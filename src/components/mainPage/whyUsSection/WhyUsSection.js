c7912004c3160025d9c65b1527fcb9c2698d80c4
Roman Verzhbovskyi <58361292+romanitwork@users.noreply.github.com>
2020-06-26 12:11:20 +0300

    Whyus (#12)
    * html stack for sections
    
    * fixed fucking classnames

--- src/components/mainPage/whyUsSection/WhyUsSection.js
+++ src/components/mainPage/whyUsSection/WhyUsSection.js
@@ -3,57 +3,57 @@ import styles from "./whyussection.module.css";
 
 const WhyUsSection = () => {
   return (
-    <section className={styles.whyus_container}>
-      <h2 className={styles.whyus_title}>Why work with us</h2>
-      <ul className={styles.whyus_list}>
-        <li className={styles.whyus_item}>
-          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
-          <p className={styles.whyus_item_text}>
+    <section className={styles.whyUsContainer}>
+      <h2 className={styles.whyUsTitle}>Why work with us</h2>
+      <ul className={styles.whyUsList}>
+        <li className={styles.whyUsItem}>
+          <h3 className={styles.whyUsItemTitle}>Lorem ipsum dolor</h3>
+          <p className={styles.whyUsItemText}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
         </li>
-        <li className={styles.whyus_item}>
-          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
-          <p className={styles.whyus_item_text}>
+        <li className={styles.whyUsItem}>
+          <h3 className={styles.whyUsItemTitle}>Lorem ipsum dolor</h3>
+          <p className={styles.whyUsItemText}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
         </li>
-        <li className={styles.whyus_item}>
-          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
-          <p className={styles.whyus_item_text}>
+        <li className={styles.whyUsItem}>
+          <h3 className={styles.whyUsItemTitle}>Lorem ipsum dolor</h3>
+          <p className={styles.whyUsItemText}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
         </li>
-        <li className={styles.whyus_item}>
-          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
-          <p className={styles.whyus_item_text}>
+        <li className={styles.whyUsItem}>
+          <h3 className={styles.whyUsItemTitle}>Lorem ipsum dolor</h3>
+          <p className={styles.whyUsItemText}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
         </li>
-        <li className={styles.whyus_item}>
-          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
-          <p className={styles.whyus_item_text}>
+        <li className={styles.whyUsItem}>
+          <h3 className={styles.whyUsItemTitle}>Lorem ipsum dolor</h3>
+          <p className={styles.whyUsItemText}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
         </li>
-        <li className={styles.whyus_item}>
-          <h3 className={styles.whyus_item_title}>Lorem ipsum dolor</h3>
-          <p className={styles.whyus_item_text}>
+        <li className={styles.whyUsItem}>
+          <h3 className={styles.whyUsItemTitle}>Lorem ipsum dolor</h3>
+          <p className={styles.whyUsItemText}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
