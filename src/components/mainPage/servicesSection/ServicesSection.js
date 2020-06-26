abd3a839db4d64caf95877c372850cbed30674f7
Betrayer <58057555+Betrayer@users.noreply.github.com>
2020-06-26 11:36:01 +0300

    Bohdan branch (#10)
    * test
    
    * test
    
    * done sectioning
    
    * container fixed
    
    * router test
    
    * oh my blog
    
    * OH MY
    
    * blog buttons
    
    * services

--- src/components/mainPage/servicesSection/ServicesSection.js
+++ src/components/mainPage/servicesSection/ServicesSection.js
@@ -5,7 +5,46 @@ const ServicesSection = () => {
   return (
     <section className={css.servicesSection}>
       <div className={css.contentWrapper}>
-        <p>I am test</p>
+        <h2 className={css.servicesHeader}>Что мы предлагаем</h2>
+        <ul className={css.servicesList}>
+          <li className={css.servicesListItem}>
+            <img
+              className={css.servicesImage}
+              src={require("../../../assets/services/tools.svg")}
+              alt="services-icon"
+              height={50}
+              width={"auto"}
+            />
+            <p className={css.servicesText}>Наши прекрасные услуги по дизайну</p>
+          </li>
+          <li className={css.servicesListItem}>
+            <img
+              src={require("../../../assets/services/search.svg")}
+              alt="services-icon"
+              height={50}
+              width={"auto"}
+            />
+            <p>Наши прекрасные услуги по продвижению в поисковиках</p>
+          </li>
+          <li className={css.servicesListItem}>
+            <img
+              src={require("../../../assets/services/document-text.svg")}
+              alt="services-icon"
+              height={50}
+              width={"auto"}
+            />
+            <p>Наши прекрасные услуги по копирайту</p>
+          </li>
+          <li className={css.servicesListItem}>
+            <img
+              src={require("../../../assets/services/document-code.svg")}
+              alt="services-icon"
+              height={50}
+              width={"auto"}
+            />
+            <p>Наши прекрасные услуги по великолепному коду</p>
+          </li>
+        </ul>
       </div>
     </section>
   );
