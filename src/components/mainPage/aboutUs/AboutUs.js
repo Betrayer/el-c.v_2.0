506b439773b62c4a2e805d74b70a1bec32e8b52a
Ellie Yelizarieva <ellie@Ellies-MacBook-Pro.local>
2020-07-07 16:49:22 +0300

    new styles, new services, toucan fixed, lang switcher

--- src/components/mainPage/aboutUs/AboutUs.js
+++ src/components/mainPage/aboutUs/AboutUs.js
@@ -25,22 +25,13 @@ const AboutUs = () => {
             <p className={styles.aboutListText}>успешной работы</p>
           </li>
           <li className={styles.aboutListItem}>
-          <div className={styles.lineBlue}></div>
-            <div className={styles.lineLightBlue}></div>
-            <div className={styles.lineYellow}></div>
-            <div className={styles.lineBlueTwo}></div>
-            <div className={styles.lineLightBlueTwo}></div>
-            <div className={styles.lineYellowTwo}></div>
+            <div className={styles.lineBlue}></div>
+            
             <h3 className={styles.aboutListTitle}>99%</h3>
             <p className={styles.aboutListText}>удовлетворённости заказчика</p>
           </li>
           <li className={styles.aboutListItem}>
-          <div className={styles.lineBlue}></div>
-            <div className={styles.lineLightBlue}></div>
-            <div className={styles.lineYellow}></div>
-            <div className={styles.lineBlueTwo}></div>
-            <div className={styles.lineLightBlueTwo}></div>
-            <div className={styles.lineYellowTwo}></div>
+            <div className={styles.lineBlue}></div>
             <h3 className={styles.aboutListTitle}>100%</h3>
             <p className={styles.aboutListText}>соблюдение сроков</p>
           </li>
