d0a947442f41651ace1ac5e0cdf543e017fe1f4f
Betrayer <StarDestroyer@ukr.net>
2020-07-14 10:49:35 +0300

    minor fix

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -2,14 +2,9 @@ import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 
 const lazyMain = lazy(
-  () => import("../mainPage/MainPage.js") /* webpackChunkName: 'Main' */
+  () => import("../mainPage/mainPage") /* webpackChunkName: 'Main' */
 );
-// const lazy404 = lazy(
-//   () =>
-//     import(
-//       "../pages/pageNotFound/PageNotFound.js"
-//     ) /* webpackChunkName: '404' */
-// );
+
 const lazyThankYou = lazy(
   () => import("../pages/thankyouPage/thankyouPage.js") /* webpackChunkName: 'Thankyou' */
 );
@@ -26,7 +21,6 @@ export const useRouter = () => {
     >
       <Switch>
         <Route exact path="/" component={lazyMain} />
-        {/* <Route path="*" component={lazy404} /> */}
         <Route exact path='/gratitude' component={lazyThankYou} />
       </Switch>
     </Suspense>
