702f14efa5e844908116f06dcdfd86950e56781a
romanitwork <workromanmail@gmail.com>
2020-07-17 09:56:08 +0300

    merging

--- src/components/pages/thankyouPage/thankyouPage.js
+++ src/components/pages/thankyouPage/thankyouPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from './thankyouPage.module.css'
 
-const thankyouPage = () => {
+const ThankyouPage = () => {
     return (
-        <>
-            I am thank you page!
-        </>
+        <div className={styles.thankyouWrapper}>
+            <h1 className={styles.thankyouTitle}>Спасибо за заявку!</h1>
+            <h2 className={styles.thankyouText}>Наши специалисты свяжутся с Вами в ближайшее время</h2>
+            <p className={styles.thankyouSocialText}>Вступайте в наши группы в социальных сетях</p>
+        </div>
     );
 }
 
-export default thankyouPage;
+export default ThankyouPage;
