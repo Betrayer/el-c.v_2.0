963bc8a2e8d71fa4b2c7e1e0898a4fec03408b0c
romanitwork <workromanmail@gmail.com>
2020-07-31 09:45:42 +0300

    merging

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -8,7 +8,7 @@ const lazyMain = lazy(
 const lazyThankYou = lazy(
   () =>
     import(
-      "../pages/thankyouPage/ThankyouPage"
+      "../pages/thankyouPage/ThankyouPage.js"
     ) /* webpackChunkName: 'Thankyou' */
 );
 
